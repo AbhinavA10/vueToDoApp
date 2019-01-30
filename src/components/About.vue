927ed5c33722e414f5609e1e0dd6c50abb2ceb26
Abhinav <abhiraja1012@gmail.com>
2019-01-29 23:19:42 -0500

    integrated vue

---
+++ src/components/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>Hi {{their_name}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "About",
+  data() {
+    return {
+      their_name: this.$route.params.name
+    };
+  }
+};
+</script>
---
+++ src/components/Skills.vue
@@ -0,0 +1,122 @@
+<template>
+  <!--In templates, can put html, vue interpolation and vue directives-->
+  <div class="hello">
+    <div class="holder">
+      <form @submit.prevent="addSkill">
+        <input
+          type="text"
+          placeholder="Enter a skill you have..."
+          v-model="skill"
+          v-validate="'min:3'"
+          name="skill"
+        >
+        <transition
+          name="alert-in"
+          enter-active-class="animated flipInX"
+          leave-active-class="animated flipOutX"
+        >
+          <p class="alert" v-if="errors.has('skill')">{{errors.first('skill')}}</p>
+        </transition>
+      </form>
+      <ul>
+        <transition-group
+          name="list"
+          enter-active-class="animated bounceInUp"
+          leave-active-class="animated bounceOutDown"
+        >
+          <li v-for="(data, index) in skills" :key="index+0">
+            {{data.skill}}
+            <i class="fa fa-minus-circle" v-on:click="remove(index)"></i>
+          </li>
+        </transition-group>
+      </ul>
+      <p>These are the skills that you possess.</p>
+    </div>
+    <!--the v-on is a vue-directive-->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Skills",
+  data() {
+    return {
+      skill: "",
+      skills: [
+        { skill: "Vue.js" },
+        { skill: "Front end dev" },
+        { skill: "nice" }
+      ]
+    };
+  },
+  methods: {
+    addSkill() {
+      this.$validator.validateAll().then(result => {
+        if (result) {
+          // validation is sucessful
+          this.skills.push({ skill: this.skill });
+          this.skill = "";
+        } else {
+          console.log("not valid");
+        }
+      });
+    },
+    remove(id) {
+      this.skills.splice(id, 1); // remove from the array
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
+@import "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";
+.holder {
+  background: #fff;
+}
+
+ul {
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+}
+
+ul li {
+  padding: 20px;
+  font-size: 1.3em;
+  background-color: #e0edf4;
+  border-left: 5px solid #3eb3f6;
+  margin-bottom: 2px;
+  color: #3e5252;
+}
+
+p {
+  text-align: center;
+  padding: 30px 0;
+  color: gray;
+}
+
+.container {
+  box-shadow: 0px 0px 40px lightgray;
+}
+input {
+  width: calc(100% - 40px);
+  border: 0;
+  padding: 20px;
+  font-size: 1.3em;
+  background-color: #323333;
+  color: #687f7f;
+}
+.alert {
+  background: #fdf2ce;
+  font-weight: bold;
+  display: inline-block;
+  padding: 5px;
+  margin-top: -20px;
+}
+i {
+  float: right;
+  cursor: pointer;
+}
+</style>
